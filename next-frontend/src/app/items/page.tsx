f9f3420d37deeefd12678b8b77a7d9cc7aadca48
aumputthipong <aumputthipong2002@gmail.com>
2025-06-12 23:05:03 +0700

    Add Timer

--- next-frontend/src/app/items/page.tsx
+++ next-frontend/src/app/items/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Typography, List, ListItem, ListItemText, Button } from '@mui/material';
 
 type Item = {
@@ -10,6 +10,35 @@ type Item = {
 
 const Page = () => {
   const [items, setItems] = React.useState<Item[]>([]);
+  const [timeLeft, setTimeLeft] =useState<number | null>(null);;
+  const [active, setActive] =  useState(false);
+
+  const startDeal = () => {
+    setTimeLeft(5 * 60); // 5 นาที (300 วินาที)
+    setActive(true);
+  };
+  
+  useEffect(() => {
+    if (!active || timeLeft === null) return;
+
+    if (timeLeft <= 0) {
+      setActive(false);
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimeLeft((prev) => (prev !== null ? prev - 1 : null));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [active, timeLeft]);
+
+
+  const formatTime = (seconds: number) => {
+    const m = Math.floor(seconds / 60).toString().padStart(2, '0');
+    const s = (seconds % 60).toString().padStart(2, '0');
+    return `${m}:${s}`;
+  };
 
   useEffect(() => {
     fetch('/api/items')
@@ -44,6 +73,18 @@ const Page = () => {
           </ListItem>
         ))}
       </List>
+      <div>
+               {active && timeLeft !== null && (
+        <h2>⏳ Time left: {formatTime(timeLeft)}</h2>
+      )}
+
+      {!active && timeLeft === 0 && <h2>❌ Deal expired</h2>}
+        <Button
+          variant="outlined" onClick={startDeal} disabled={active}>
+            Deal
+          </Button>
+       
+      </div>
     </Box>
   );
 };
