d086bc312ffec86e4d8908c7a2d879d982c2f579
aumputthipong <aumputthipong2002@gmail.com>
2025-06-13 22:50:05 +0700

    get post

--- go-backend/main.go
+++ go-backend/main.go
@@ -3,7 +3,39 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+  "encoding/json"
 )
+
+type User struct{
+  ID int `json:id`
+  Name string `json:"name"`
+
+}
+
+var users= []User{
+	{ID: 1, Name: "Alice"},
+	{ID: 2, Name: "Bob"},
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(users)
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	newUser.ID = len(users) + 1
+	users = append(users, newUser)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newUser)
+}
 
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
